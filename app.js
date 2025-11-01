76e6087f6267635cc1635bca375b1df9ef85d6ea
温良 <14356373+wenliang_f@user.noreply.gitee.com>
2025-11-02 01:23:42 +0800

    更改:文件上传体积限制放宽

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
